5ee1537af7c9917bf5aab95232373e0bfd09caf6
AaKaShhhhhhhhh <aakashkamal47@gmail.com>
2026-02-06 16:25:15 +0530

    spacing fixed somewhat

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Desktop from './components/Desktop';
 import './styles/main.css';
 import './styles/sleek-ui.css';
+import './styles/foxie-tech.css';
 import './styles/desktop-shell.css';
 
 function App() {
